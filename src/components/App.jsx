848e62b7e2ee2d76c9aeaf17e69b897e08f35921
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-12-28 01:21:23 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,29 +16,71 @@ export class App extends Component {
     modal: false,
     selectedPicture: "",
     hasMorePictures: false,
-  
   };
-  
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
- 
     const searchValue = e.target.searchInput.value;
-    this.setState({
-      isLoading: true,
-      pictures: [],
-      page: 1
-    }, () => {
+    this.setState(
+      {
+        isLoading: true,
+        pictures: [],
+        page: 1
+      },
+      () => {
+        fetch(`https://pixabay.com/api/?q=${searchValue}&page=${this.state.page}&key=${ApiKey}&image_type=photo&orientation=horizontal&per_page=${this.state.perPage}`)
+          .then((res) => res.json())
+          .then((data) => {
+            this.setState((prev) => ({
+              pictures: data.hits,
+              page: prev.page + 1,
+              isLoading: false,
+            }));
+
+            if (data.hits.length === 12) {
+              this.setState({
+                hasMorePictures: true,
+              });
+            } else {
+              this.setState({
+                hasMorePictures: false,
+              });
+            }
+            console.log(this.state.page);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
+    );
+  };
+
+  loadMore = () => {
+    const searchInput = document.querySelector('[name="searchInput"]');
+    const searchValue = searchInput.value;
+
     fetch(`https://pixabay.com/api/?q=${searchValue}&page=${this.state.page}&key=${ApiKey}&image_type=photo&orientation=horizontal&per_page=${this.state.perPage}`)
-      .then(res => res.json())
-      .then(data => {
-       
-          this.setState(prev => ({
-            pictures: data.hits,
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState((prev) => ({
+          isLoadingMore: true,
+        }));
+        setTimeout(() => {
+          this.setState((prev) => ({
+            pictures: [...prev.pictures, ...data.hits],
             page: prev.page + 1,
-            isLoading: false,
+            isLoadingMore: false,
           }));
-        
-  
+        }, 300);
+
         if (data.hits.length === 12) {
           this.setState({
             hasMorePictures: true,
@@ -48,69 +90,33 @@ export class App extends Component {
             hasMorePictures: false,
           });
         }
-        console.log(this.state.page)
       })
-      .catch(error => {
-        console.error(error);
-      });
-    })
-  };
-  
-  
-  
-
-
-  loadMore = () => {
-    const searchInput = document.querySelector('[name="searchInput"]');
-    const searchValue = searchInput.value;
-    
-    fetch(`https://pixabay.com/api/?q=${searchValue}&page=${this.state.page}&key=${ApiKey}&image_type=photo&orientation=horizontal&per_page=${this.state.perPage}`)
-      .then(res => res.json())
-      .then(data => {
-        this.setState(prev => ({
-          isLoadingMore: true,
-        }));
-        setTimeout(() => {
-          this.setState(prev => ({
-            pictures: [...prev.pictures, ...data.hits],
-            page: prev.page + 1,
-            isLoadingMore: false,
-          }));
-        }, 300);
-      
-      if (data.hits.length === 12) {
-        this.setState({
-          hasMorePictures: true,
-        });
-      } else {
-        this.setState({
-          hasMorePictures: false,
-        });
-      }})
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   };
-  
+
   toggleModal = (e) => {
     const selectedPicture = e.target.src;
     this.setState({
       modal: true,
       selectedPicture: selectedPicture,
     });
-   
   };
-  closeModal = () =>{
+
+  closeModal = () => {
     this.setState({
       modal: false,
-    })
-  }
- handleKeyDown = (event) => {
-        if (event.key === "Escape") {
-            this.setState({
-              modal: false,
-            });
-          }}
+    });
+  };
+
+  handleKeyDown = (event) => {
+    if (event.key === "Escape") {
+      this.setState({
+        modal: false,
+      });
+    }
+  };
 
   render() {
     const { pictures, isLoading, modal, selectedPicture, isLoadingMore, hasMorePictures } = this.state;
